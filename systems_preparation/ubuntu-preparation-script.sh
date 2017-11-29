7f0ea98a726f11eb008b3ca9a8593a21b6c67cfa
Yuri Motta <ymlrs.eng@gmail.com>
2017-11-29 07:43:34 -0400

    Atualizado o ubuntu-preparation-script. Foi adicionada a instalacao do python pip.

--- systems_preparation/ubuntu-preparation-script.sh
+++ systems_preparation/ubuntu-preparation-script.sh
@@ -6,6 +6,8 @@ echo "installing ffmpeg library"
 sudo apt-get install ffmpeg
 echo "installing python tkinter library"
 sudo apt-get install python-tk
+echo "installing python pip"
+sudo apt-get install python-pip
 echo "installing pillow library"
 sudo pip install pillow
 
