3a7ce989b0bd48655187fe57d2937dc7f3b1e6f0
Yuri Motta <ymlrs.eng@gmail.com>
2017-12-15 10:36:47 -0400

    foi adicionado o software joystick control interface. Com ele e possivel utilizar o joystick para controlar a cadeira de rodas. As mensagens ja estao de acordo com o server_iepw_udoo. Ja esta incluido o RTT. Essa versao foi testada em rede local, e funcionou corretamente.

--- Final-Software/joystick-interface/joystick_interface-linux.py
+++ Final-Software/joystick-interface/joystick_interface-linux.py
@@ -5,6 +5,7 @@ import time
 import threading
 from threading import Thread
 import datetime
+import serial
 from PIL import ImageTk
 
 """ NOVA VERSAO DA GUI CLIENT"""
@@ -34,17 +35,8 @@ class Application(Frame):
     def create_widgets(self):
 
         # instructions
-        self.instructions = Label(self, text="Wheelchair Training \n Environment", font="bold")
+        self.instructions = Label(self, text="Wheelchair Training \n Environment \n Joystick control interface", font="bold")
         self.instructions.grid(row=0, column=1, sticky=W)
-
-        #gui icon
-        self.gui_icon = Label(self)
-        self.wheelchair_img = ImageTk.PhotoImage(file="icons/gui_icon.png")
-        self.gui_icon.config(image=self.wheelchair_img)
-        self.gui_icon.image = self.wheelchair_img
-        self.gui_icon.pack
-        self.gui_icon.grid(row=1,column=1, sticky=W)
-
 
         # ip entry
         #Label(self, text="Command Server IP: " + ip_command_server).grid(row=6, column=1, sticky=W)
@@ -59,49 +51,6 @@ class Application(Frame):
         # Stop training
         self.button_stop = Button(self, text="Stop training", command=self.stop_training, bg='#DF0101')
         self.button_stop.grid(row=6, column=1, sticky=W)
-
-        #instructions
-        Label(self, text="Commands").grid(row=0, column=9, sticky=W)
-
-        #UP button
-        self.button_up = Button(self, text="UP", command=self.up_command)
-        self.up_image = ImageTk.PhotoImage(file="icons/up_button.png")
-        self.button_up.config(image=self.up_image)
-        self.button_up.image =self.up_image
-        self.button_up.pack
-        self.button_up.grid(row=1, column =9)
-
-        #DOWN button
-        self.button_down = Button(self, text="DOWN", command=self.down_command)
-        self.down_image = ImageTk.PhotoImage(file="icons/down_button.png")
-        self.button_down.config(image=self.down_image)
-        self.button_down.image = self.down_image
-        self.button_down.pack
-        self.button_down.grid(row=3, column=9)
-
-        #LEFT button
-        self.button_left = Button(self, text="LEFT", command=self.left_command)
-        self.left_image = ImageTk.PhotoImage(file="icons/left_button.png")
-        self.button_left.config(image=self.left_image)
-        self.button_left.image = self.left_image
-        self.button_left.pack
-        self.button_left.grid(row=2, column=8)
-
-        #RIGHT button
-        self.button_right = Button(self, text="RIGHT", command=self.right_command)
-        self.right_image = ImageTk.PhotoImage(file="icons/right_button.png")
-        self.button_right.config(image=self.right_image)
-        self.button_right.image = self.right_image
-        self.button_right.pack
-        self.button_right.grid(row=2,column=10)
-
-        #STOP button
-        self.button_stop = Button(self, text="STOP", command=self.stop_command)
-        self.stop_image = ImageTk.PhotoImage(file="icons/stop_button.png")
-        self.button_stop.config(image=self.stop_image)
-        self.button_stop.image = self.stop_image
-        self.button_stop.pack
-        self.button_stop.grid(row=2, column=9)
 
 
     # start training
@@ -120,84 +69,28 @@ class Application(Frame):
         self.t1.setDaemon(True)
         self.t1.start()
 
+        ## thread video streamin
         video_stream = client_video_stream()
         self.t2 = threading.Thread(target=video_stream.run)
         self.t2.setDaemon(True)
         self.t2.start()
+
+        ## thread joystick
+        joystick = joystick_sender()
+        self.t3 = threading.Thread(target=joystick.run)
+        self.t3.setDaemon(True)
+        self.t3.start()
 
     def stop_training(self):
         global stop_flag
 
         command = "stop"
         conn.sendto(command, (ip_server, command_port))
-
+        conn.sendto(command, (ip_server, command_port))
+        conn.sendto(command, (ip_server, command_port))
         stop_flag = True
         print "Stop training"
         Label(self, text="       Training finished        " + "\n" +"      Thank you!       ").grid(row=7, column=1, sticky=W)
-
-
-    def up_command(self):
-        command = "X=-55%,Y=80%"
-        if updated==True:
-            conn.sendto(command, (ip_server, command_port))
-            conn.sendto(command, (ip_server, command_port))
-            conn.sendto(command, (ip_server, command_port))
-            conn.sendto(command, (ip_server, command_port))
-            conn.sendto(command, (ip_server, command_port))
-
-        else:
-            Label(self, text="First press Start", fg='red').grid(row=6, column=1, sticky=W)
-
-    def down_command(self):
-        command = "X=0%,Y=-80%"
-        if updated == True:
-            conn.sendto(command, (ip_server, command_port))
-            conn.sendto(command, (ip_server, command_port))
-            conn.sendto(command, (ip_server, command_port))
-            conn.sendto(command, (ip_server, command_port))
-            conn.sendto(command, (ip_server, command_port))
-
-        else:
-            Label(self, text="First press Start", fg='red').grid(row=6, column=1, sticky=W)
-
-    def left_command(self):
-        command = "X=80%,Y=0%"
-        if updated == True:
-            conn.sendto(command, (ip_server, command_port))
-            conn.sendto(command, (ip_server, command_port))
-            conn.sendto(command, (ip_server, command_port))
-            conn.sendto(command, (ip_server, command_port))
-            conn.sendto(command, (ip_server, command_port))
-
-
-        else:
-            Label(self, text="First press Start", fg='red').grid(row=6, column=1, sticky=W)
-
-    def right_command(self):
-        command = "X=-83%,Y=0%"
-        if updated == True:
-            conn.sendto(command, (ip_server, command_port))
-            conn.sendto(command, (ip_server, command_port))
-            conn.sendto(command, (ip_server, command_port))
-            conn.sendto(command, (ip_server, command_port))
-            conn.sendto(command, (ip_server, command_port))
-
-
-        else:
-            Label(self, text="First press Start", fg='red').grid(row=6, column=1, sticky=W)
-
-    def stop_command(self):
-        command = "X=0%,Y=0%"
-        if updated == True:
-            conn.sendto(command, (ip_server, command_port))
-            conn.sendto(command, (ip_server, command_port))
-            conn.sendto(command, (ip_server, command_port))
-            conn.sendto(command, (ip_server, command_port))
-            conn.sendto(command, (ip_server, command_port))
-
-
-        else:
-            Label(self, text="First press Start", fg='red').grid(row=6, column=1, sticky=W)
 
 
 class client_video_stream(Thread):
@@ -252,10 +145,117 @@ class RTT_sender(Thread):
             except AttributeError:
                 print "failed to ping"
 
+class joystick_sender(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+
+    def run(self):
+        global conn, updated, ip_command_server, port_command_server
+
+        """INICIA OS VALORES DE X E Y"""
+        X = "0"
+        Y = "0"
+
+        """CONSTANTES"""
+        # PARA X
+        valor_central_max_X = 495
+        valor_central_min_X = 480
+        valor_max_X = 710
+        valor_min_X = 290
+        max_variacao_posit_X = valor_max_X - valor_central_max_X
+        max_variacao_neg_X = valor_central_min_X - valor_min_X
+
+        # PARA Y
+        valor_central_max_Y = 535
+        valor_central_min_Y = 510
+        valor_max_Y = 730
+        valor_min_Y = 295
+        max_variacao_posit_Y = valor_max_Y - valor_central_max_Y
+        max_variacao_neg_Y = valor_central_min_Y - valor_min_Y
+
+        # PORTA SERIAL UTILIZADA
+        ser = serial.Serial('/dev/ttyACM0', 9600)
+
+        try:
+            while True:
+                data = ser.readline(5)
+                vetor_mensagem = data.split(":", 1)
+                if vetor_mensagem[0] == "X":
+                    X = vetor_mensagem[1]
+                if vetor_mensagem[0] == "Y":
+                    Y = vetor_mensagem[1]
+
+                # mensagem_final = "X:" + X + ",Y:" + Y
+                # print mensagem_final
+                # print data
+
+                """CONVERSAO DE 10 BITS PARA PROTOCOLO DE MENSAGEM"""
+                X = float(X)
+                Y = float(Y)
+
+                """PARA X"""
+                # valor central de X
+                if X >= valor_central_min_X and X <= valor_central_max_X:
+                    X_msg = 0
+
+                # intervalo positivo de X
+                if X > valor_central_max_X:
+                    X_msg = int(round(((X - valor_central_max_X) / max_variacao_posit_X) * 100))
+
+                # intervalo negativo de X
+                if X < valor_central_min_X:
+                    X_msg = int(round(((X - valor_central_min_X) / max_variacao_neg_X) * 100))
+
+                """PARA Y """
+                # valor central de Y
+                if Y >= valor_central_min_Y and Y <= valor_central_max_Y:
+                    Y_msg = 0
+
+                # intervalo positivo de Y
+                if Y > valor_central_max_Y:
+                    Y_msg = int(round(((Y - valor_central_max_Y) / max_variacao_posit_Y) * 100))
+
+                # intervalo negativo de Y
+                if Y < valor_central_min_Y:
+                    Y_msg = int(round(((Y - valor_central_min_Y) / max_variacao_neg_Y) * 100))
+
+                """ CONDICAO PARA X,Y FICAREM ENTRE -100 E 100"""
+                if X_msg >100:
+                    X_msg =100
+
+                if X_msg <-100:
+                    X_msg = -100
+
+                if Y_msg > 100:
+                    Y_msg = 100
+
+                if Y_msg < -100:
+                    Y_msg = -100
+
+                self.command = "X=" + str(X_msg) + "%" + ",Y=" + str(Y_msg) + "%"
+                print self.command
+                # print data
+
+                """ SALVA O TIMESTAMP EM QUE O COMANDO FOI EXECUTADO"""
+                ts = time.time()
+                ano = datetime.datetime.fromtimestamp(ts).strftime('%Y')
+                mes = datetime.datetime.fromtimestamp(ts).strftime('%m')
+                dia = datetime.datetime.fromtimestamp(ts).strftime('%d')
+                hora = datetime.datetime.fromtimestamp(ts).strftime('%H')
+                minutos = datetime.datetime.fromtimestamp(ts).strftime('%M')
+                segundos = datetime.datetime.fromtimestamp(ts).strftime('%S')
+
+
+                """ENVIO DE MENSAGEM COMANDO VIA SOCKET UDP"""
+                conn.sendto(self.command, (ip_command_server, port_command_server))
+        except KeyboardInterrupt:
+            pass
+
+
 
 root = Tk()
-root.title("CLIENT GUI")
-root.geometry("400x360")
+root.title("Joystick Interface")
+root.geometry("300x200")
 app = Application(root)
 
 root.mainloop()
